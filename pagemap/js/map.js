3ff666e02cacd2969e163b9742a7611209774e84
serikyu <lbuendia@ucsc.edu>
2025-05-25 13:55:03 -0700

    apikey

--- pagemap/js/map.js
+++ pagemap/js/map.js
@@ -5,13 +5,13 @@
 let myMap = L.map("map").setView([36.99, -122.06], 15);
 
 // Use the Stadia OSM Bright tile layer (From https://leaflet-extras.github.io/leaflet-providers/preview/)
-let Stadia_OSMBright = L.tileLayer('https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.{ext}', {
+let Stadia_OSMBright = L.tileLayer('https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.{ext}?api_key=e17bade-7489-48bd-8ccb-381c575921de', {
 	minZoom: 0,
 	maxZoom: 20,
-	attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	attribution: '&copy; Stadia Maps, &copy; OpenMapTiles &copy; OpenStreetMap contributors',
 	ext: 'png'
-});
-
+  });
+  
 // Implement tile layer style
 Stadia_OSMBright.addTo(myMap);
 
