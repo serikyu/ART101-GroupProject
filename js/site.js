58345f5373883c0998af151090da697e6d1660c4
AlexanderLA17 <80607202+AlexanderLA17@users.noreply.github.com>
2025-06-09 16:59:48 -0700

    bug fix test - Alexander
    bug fix test

--- js/site.js
+++ js/site.js
@@ -51,7 +51,7 @@ document.addEventListener('DOMContentLoaded', function() {
   const image2 = document.querySelector('.aboutslug-hover-image');
   
   image2.addEventListener('mouseover', function() {
-    this.src = "./img/heartslug2.png";
+    this.src = "../img/heartslug2.png";
   });
   
   image2.addEventListener('mouseout', function() {
